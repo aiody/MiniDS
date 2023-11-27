efb13ac2a1dcfb9a9e3e8ee4547dfb5a990c9c8d
Robin <aiody329@gmail.com>
2023-11-27 20:41:32 +0900

    Initial commit

---
+++ Source/MiniDS/MiniDS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiniDS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiniDS, "MiniDS" );
---
+++ Source/MiniDS/MiniDSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MiniDSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIDS_API AMiniDSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
