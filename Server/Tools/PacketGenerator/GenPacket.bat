80336af55120ada5a0a7de85e955ab2370570a5d
Robin <aiody329@gmail.com>
2024-07-11 12:04:02 +0900

    코드 파일 구조 변경

--- Server/Tools/PacketGenerator/GenPacket.bat
+++ Server/Tools/PacketGenerator/GenPacket.bat
@@ -5,12 +5,12 @@ START /WAIT ../../Plugins/protobuf/protoc.exe -I=./ --cpp_out=./ ./Protocol.prot
 IF NOT ERRORLEVEL 0 PAUSE
 
 :: for server
-XCOPY Enum.pb.h "../.." /Y
-XCOPY Struct.pb.h "../.." /Y
-XCOPY Protocol.pb.h "../.." /Y
-XCOPY Enum.pb.cc "../.." /Y
-XCOPY Struct.pb.cc "../.." /Y
-XCOPY Protocol.pb.cc "../.." /Y
+XCOPY Enum.pb.h "../../Codes/Protocol" /Y
+XCOPY Struct.pb.h "../../Codes/Protocol" /Y
+XCOPY Protocol.pb.h "../../Codes/Protocol" /Y
+XCOPY Enum.pb.cc "../../Codes/Protocol" /Y
+XCOPY Struct.pb.cc "../../Codes/Protocol" /Y
+XCOPY Protocol.pb.cc "../../Codes/Protocol" /Y
 
 :: for client
 XCOPY Enum.pb.h "../../../Source/MiniDS/Network/Protocol" /Y
